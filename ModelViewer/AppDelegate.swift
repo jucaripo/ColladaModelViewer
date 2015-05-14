31eb21b0d036fbc199f9d699cb7605d3688c7baf
Juan Carlos Ricalde Poveda <jucaripo@hotmail.co>
2015-05-14 12:06:39 -0500

    visor collada demo

---
+++ ModelViewer/AppDelegate.swift
@@ -0,0 +1,18 @@
+//
+//  AppDelegate.swift
+//  ModelViewer
+//
+//  Created by Jucaripo on 02/05/15.
+//  Copyright (c) 2014 FricAnimation. All rights reserved.
+//  Presentacion de NScoder México
+
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+                            
+    var window: UIWindow?
+
+    
+}
+
---
+++ ModelViewer/GameViewController.swift
@@ -0,0 +1,23 @@
+//
+//  GameViewController.swift
+//  ModelViewer
+//
+//  Created by Jucaripo on 02/05/15.
+//  Copyright (c) 2014 FricAnimation. All rights reserved.
+//  Presentacion de NScoder México
+
+import UIKit
+import SceneKit
+
+class GameViewController: UIViewController {
+    @IBOutlet weak var sceneView: SCNView!
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // ejemplo animado
+        //sceneView.scene = SCNScene(named: "art.scnassets/animado");
+        // ejemplo textura
+        sceneView.scene = SCNScene(named: "art.scnassets/mushroom");
+        // ejemplo objeto   sceneView.scene = SCNScene(named: "art.scnassets/sword");
+    }
+}
